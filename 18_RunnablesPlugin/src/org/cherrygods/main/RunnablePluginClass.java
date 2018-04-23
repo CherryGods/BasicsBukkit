8b14b8b821632e414a8c30b0f0976d87f0c12956
CherryGods <the_lty_mail@foxmail.com>
2018-04-23 10:51:06 +0800

    实现Runnable任务调度

--- 18_RunnablesPlugin/src/org/cherrygods/main/RunnablePluginClass.java
+++ 18_RunnablesPlugin/src/org/cherrygods/main/RunnablePluginClass.java
@@ -1,8 +1,10 @@
 package org.cherrygods.main;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.cherrygods.utils.RunnablePluginPrefix;
 
 /**
@@ -21,6 +23,38 @@ public class RunnablePluginClass extends JavaPlugin implements Listener {
     public void onEnable() {
         getPrefix = new RunnablePluginPrefix();
         getServer().getConsoleSender().sendMessage(getPrefix.cmdPrefix+ChatColor.AQUA+"has been Enable");
+        runnable();
+        runnableDelayed();
         super.onEnable();
+    }
+    public void runnable() {
+        new BukkitRunnable() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void run() {
+                for (LivingEntity e : getServer().getWorld("world").getLivingEntities()) {
+
+                    e.setCustomName(e.getType() + "ยง4[" + ChatColor.RED + e.getHealth()
+                            + "ยง8/ยงc" + e.getMaxHealth() + "ยง4]");
+                    e.setCustomNameVisible(true);
+
+                }
+
+            }
+
+        }.runTaskTimerAsynchronously(this, 0, 5);
+
+    }
+
+    public void runnableDelayed() {
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                getServer().broadcastMessage(ChatColor.GREEN + "Server Has Started");
+            }
+
+        }.runTaskLaterAsynchronously(this, 40);
     }
 }
