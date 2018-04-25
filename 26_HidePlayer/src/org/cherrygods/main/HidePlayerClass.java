c535e5ecbcd680cd2cd2fd5e53cc29d68e462bad
CherryGods <the_lty_mail@foxmail.com>
2018-04-25 14:32:54 +0800

    emmm隐藏玩家..毫无作用

--- 26_HidePlayer/src/org/cherrygods/main/HidePlayerClass.java
+++ 26_HidePlayer/src/org/cherrygods/main/HidePlayerClass.java
@@ -2,6 +2,7 @@ package org.cherrygods.main;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.cherrygods.commands.HidePlayerCommand;
 
 /**
  * @author CherrGods
@@ -10,7 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class HidePlayerClass extends JavaPlugin {
     @Override
     public void onEnable() {
+        HidePlayerCommand hidePlayerCommand = new HidePlayerCommand();
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN+getName()+" >> "+ChatColor.AQUA+"has been Enable");
+        getCommand(hidePlayerCommand.cmd1).setExecutor(hidePlayerCommand);
         loadConfig();
         super.onEnable();
     }
