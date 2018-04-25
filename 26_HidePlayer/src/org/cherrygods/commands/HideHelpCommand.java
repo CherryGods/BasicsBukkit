e6f30cf5e2d4076ec4376c890fe5ae7faf3c05e3
CherryGods <the_lty_mail@foxmail.com>
2018-04-25 17:49:05 +0800

    指令: /<sh> <help>(查看帮助!)
    /<hide> <me>(隐藏自己)
    /<hide> <all>(隐藏所有人)
    /<hide> <player> [玩家名称](你看不见某个在线的玩家)
    
    /<show> <me>(显示自己)
    /<show> <all>(显示所有人)
    /<show> <player> [玩家名称](你能看见这个在线的玩家)
    权限:
      op:
        show.player
      default:
        hide me
        hide all
        hide player

--- 26_HidePlayer/src/org/cherrygods/commands/HideHelpCommand.java
+++ 26_HidePlayer/src/org/cherrygods/commands/HideHelpCommand.java
@@ -1,16 +1,58 @@
 package org.cherrygods.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.cherrygods.main.HidePlayerClass;
 
 /**
  * /hide help
  * @author CherrGods
  */
 public class HideHelpCommand implements CommandExecutor {
+    private HidePlayerClass plugin = HidePlayerClass.getPlugin(HidePlayerClass.class);
+    public String cmd1 = "sh";
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player){
+            Player player = (Player) commandSender;
+            if(strings.length==1){
+                if(command.getName().equalsIgnoreCase(cmd1)){
+                    if(strings[0].equalsIgnoreCase("help")){
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "+ChatColor.GOLD+"===========================================");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> " +ChatColor.LIGHT_PURPLE+"/sh help"+ChatColor.GREEN+"-----查看帮助");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/sh help"+ChatColor.GREEN+"-----查看帮助.");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/hide me"+ChatColor.GREEN+"-----隐藏自己.");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/hide all"+ChatColor.GREEN+"-----屏蔽所有人.");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/hide player [玩家名称]"+ChatColor.GREEN+"-----屏蔽玩家.");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/show me"+ChatColor.GREEN+"-----取消屏蔽自己。");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/show all"+ChatColor.GREEN+"-----取消屏蔽所有人");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                                +ChatColor.LIGHT_PURPLE+"/show player [玩家名称]"+ChatColor.GREEN+"-----取消屏蔽玩家");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> " +ChatColor.LIGHT_PURPLE+"/sh help"+ChatColor.GREEN+"-----查看帮助");
+                        player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "+ChatColor.GOLD+"===========================================");
+                        return true;
+                    }
+                    return true;
+                }
+            }else{
+                player.sendMessage(ChatColor.YELLOW+plugin.getName()+" >> "
+                        +ChatColor.DARK_RED+"你可以输入/<sh> <help>查看帮助！");
+                return true;
+            }
+        }else{
+            commandSender.sendMessage(ChatColor.GREEN+plugin.getName()+" >> "
+                    +ChatColor.DARK_RED+"你必须是一名玩家！");
+            return true;
+        }
         return false;
     }
 }
--- 26_HidePlayer/src/org/cherrygods/main/HidePlayerClass.java
+++ 26_HidePlayer/src/org/cherrygods/main/HidePlayerClass.java
@@ -2,6 +2,7 @@ package org.cherrygods.main;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.cherrygods.commands.HideHelpCommand;
 import org.cherrygods.commands.HidePlayerCommand;
 
 /**
@@ -12,9 +13,11 @@ public class HidePlayerClass extends JavaPlugin {
     @Override
     public void onEnable() {
         HidePlayerCommand hidePlayerCommand = new HidePlayerCommand();
+        HideHelpCommand hideHelpCommand = new HideHelpCommand();
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + getName() + " >> " + ChatColor.AQUA + "has been Enable");
         getCommand(hidePlayerCommand.cmd1).setExecutor(hidePlayerCommand);
         getCommand(hidePlayerCommand.cmd2).setExecutor(hidePlayerCommand);
+        getCommand(hideHelpCommand.cmd1).setExecutor(hideHelpCommand);
         loadConfig();
         super.onEnable();
     }
